cf20f6e1bcba9df044c6dd4f53fdcb418dd7e958
KoruptTinker <pisces1237@gmail.com>
2020-06-14 18:10:57 +0530

    Integrated blockchain

---
+++ src/main/java/blockchain/block_chain.java
@@ -0,0 +1,60 @@
+package main.java.blockchain;
+import java.util.ArrayList; 
+
+public class block_chain {
+	public static ArrayList<block> blockchain = new ArrayList<block>();
+	public static int difficulty = 3;
+    public static void Create_Blockchain(){
+	
+    	blockchain.add(new block("cn project", "0"));
+    	blockchain.get(0).mining(difficulty);
+    	
+    
+    
+    }
+	
+	public static void Add_Block(String data){
+		
+		blockchain.add(new block( data, blockchain.get(blockchain.size() - 1) .hash)); 
+		blockchain.get(blockchain.size() - 1).mining(difficulty);
+		
+	}
+	public static Boolean verify() {
+		
+		block current; 
+		block previous;
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+		for(int i=1; i < blockchain.size(); i++) {
+			current = blockchain.get(i);
+			previous = blockchain.get(i-1);
+			
+			if(!current.hash.equals(current.calculateHash()) ){			
+				return false;
+			}
+			if(!previous.hash.equals(current.previousHash) ) {
+				return false;
+			}
+			if(!current.hash.substring( 0, difficulty).equals(hashTarget)) {
+				System.out.println("This block hasn't been mined");
+				return false;
+			}
+		}
+		return true;
+	}
+	public static void display(){
+		
+		for(int i=0; i < blockchain.size(); i++) {
+		 System.out.print("Block"+i);	
+		 System.out.println("");
+		 System.out.print(blockchain.get(i).hash);
+		 System.out.print(" "+ blockchain.get(i).previousHash);
+		 System.out.print(" "+ blockchain.get(i).get_data());
+		 System.out.print(" "+ blockchain.get(i).get_timestamp());
+		 System.out.print(" "+ blockchain.get(i).nonce());
+		 System.out.println("");
+		}
+		
+		
+	}
+
+}
--- src/main/java/lockrypt.java
+++ src/main/java/lockrypt.java
@@ -1,23 +1,28 @@
 package main.java;
 import main.java.AdvancedEncryption;
 import main.java.fileManager;
+import main.java.blockchain.*;
 public class lockrypt {
     public static void main(String[] args) throws Exception{ 
         AdvancedEncryption AE=new AdvancedEncryption();
         fileManager FM=new fileManager();
-        byte[][] cipher=new byte[2][];
-        byte[][] decipher=new byte[2][];
+        block_chain.Create_Blockchain();
+        byte[][] cipher=new byte[1][];
+        byte[][] decipher=new byte[1][];
         for(int i=0;i<FM.fileList.length;i++){
             FM.getFileBytes(FM.fileList[i]);
         }
         for(int i=0;i<FM.fileList.length;i++){
             cipher[i]=AE.encrypt(FM.fileBytes[i]);
+            String ciphertext=new String(cipher[i]);
+            //block_chain.Add_Block(ciphertext);
             FM.deleteFile(FM.fileList[i]);
-            System.out.println(new String(cipher[i]));
+            //System.out.println(new String(cipher[i]));
         }
         for(int i=0;i<FM.fileList.length;i++){
+            
             decipher[i]=AE.decrypt(cipher[i]);
-            System.out.println(new String(decipher[i]));
+            //System.out.println(new String(decipher[i]));
         }
     }
 }
